2c3b4b1451891dedda15d00db4d2a7b231670961
Shivachauhan17 <shivanbd2019@gmail.com>
2023-03-25 18:32:36 +0530

    designing and dynamic tempaltes setup in html file

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 #__name__ refers to how a particular script is invoked
 app=Flask(__name__)
 
+Jobs=[
+    {
+    "id":1,
+    "title":"data scientist",
+    "location":"hydrabad",
+    "salary":"10,00,000"
+    },
+    {
+    "id":2,
+    "title":"data analyst",
+    "location":"Noida",
+    "salary":"9,00,000"
+    },
+    {
+    "id":3,
+    "title":"software engineer",
+    "location":"remote",
+    "salary":"11,00,000"
+    }
+]
 #to contructing  a url and if that pertucular url accessed then the function 
 # below decorator will be executed
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html',jobs=Jobs)
 
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(Jobs)
 if __name__== "__main__":
-    app.run(debug = True,port=8080,use_reloader=False)
+    app.run(debug = True,port=8000,use_reloader=False)
