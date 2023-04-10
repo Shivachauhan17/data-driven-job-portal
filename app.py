6ec01d1c3755a281c9c53259204bf51ba93be0a3
Shivachauhan17 <shivanbd2019@gmail.com>
2023-04-10 19:05:58 +0530

    making the new page for individual jobs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request,jsonify
-from database import engine
+from database import engine, load_jobs_from_db, load_job_from_db 
 from sqlalchemy import text
 #__name__ refers to how a particular script is invoked
 app=Flask(__name__)
@@ -15,13 +15,21 @@ def load_job_from_db():
         for row in result.mappings().all():
             jobs.append(dict(row))
     return jobs
+
+
 @app.route('/')
 def hello():
     jobs=load_job_from_db()
     return render_template('index.html',jobs=jobs)
 
-"""@app.route('/api/jobs')
-def list_jobs():
-    return jsonify(jobs)"""
+
+
+@app.route("/jobs/<string:id>/")
+def  show_jobs(id):
+    job=load_jobs_from_db(id)
+    if not job:
+        return "not found",404
+    return render_template("jobpage.html",job=job)
+
 if __name__== "__main__":
     app.run(debug = True,port=8800,use_reloader=False)
--- database.py
+++ database.py
@@ -9,13 +9,22 @@ engine=create_engine(url,connect_args={
         }
     })
 
-
-with engine.connect() as conn:
+def load_job_from_db():
     jobs=[]
+    with engine.connect() as conn:
+        result=conn.execute(text("select * from jobs"))
+        for row in result.mappings().all():
+            jobs.append(dict(row))
+    return jobs
 
-    result=conn.execute(text("select * from jobs"))
-    #print(type(result))
-    #print(type(result.all()))
-    for row in result.mappings().all():
-        jobs.append(dict(row))
-print(jobs)
+def load_jobs_from_db(id):
+    with engine.connect() as conn:
+        #query="SELECT * FROM jobs WHERE id = ?"
+        #result=conn.execute(query,(id,))
+
+        result=conn.execute(text("SELECT * FROM jobs WHERE id = (%val)"),id)
+        rows=result.mappings().all()
+        if len(rows)==0:
+            return None
+        else:
+            return dict(rows[0])
